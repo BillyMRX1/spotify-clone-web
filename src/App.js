f8b4c94f72a0f43deb27adca66e4a4602daa0cb0
Brilian Ade Putra <brilianadeputra@gmail.com>
2021-07-14 14:14:24 +0800

    Add Constant Data and display them using home.jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Music from './components/home/home';
 
 function App() {
-  console.log(process.env.REACT_APP_CLIENT_ID);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create Playlist</h1>
+      <Music/>
     </div>
   );
 }
